3423436692fe40a319bb5e115f88cc4fbf910ef6
FUJITA KAZUHISA <>
2022-10-21 13:56:05 +0900

    improve code + integrate with UbiquityAlgorithmicDollarManager

--- contracts/DirectUBQFarmer2.sol
+++ contracts/DirectUBQFarmer2.sol
@@ -13,21 +13,25 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./interfaces/IDepositZap.sol";
 import "./interfaces/IBondingV2.sol";
 import "./interfaces/IBondingShareV2.sol";
+import "./interfaces/IStableSwap3Pool.sol";
+import "./interfaces/IUbiquityAlgorithmicDollarManager.sol";
 import 'hardhat/console.sol';
 
 
-contract DirectUBQFarmer is ReentrancyGuard {
+contract DirectUBQFarmer2 is ReentrancyGuard {
     using SafeERC20 for IERC20;
 
-    address public constant USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;//decimal 6
-    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;//decimal 6
-    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
-    address public constant UAD3CRVf = 0x20955CB69Ae1515962177D164dfC9522feef567E;
-    address public constant UAD = 0x0F644658510c95CB46955e55D7BA9DDa9E9fBEc6;
-    address public constant UBQ = 0x4e38D89362f7e5db0096CE44ebD021c3962aA9a0;
-    address public constant DepositZapUAD = 0xA79828DF1850E8a3A3064576f380D90aECDD3359;
-    address public constant BondingV2 = 0xC251eCD9f1bD5230823F9A0F99a44A87Ddd4CA38;
-    address public constant BondingShareV2 = 0x2dA07859613C14F6f05c97eFE37B9B4F212b5eF5;
+    address public USDT;//decimal 6
+    address public USDC;//decimal 6
+    address public DAI;
+    address public UAD3CRVf;
+    address public UAD;
+    address public UBQ;
+    address public DepositZapUAD;
+    address public BondingV2;
+    address public BondingShareV2;
+
+    IUbiquityAlgorithmicDollarManager public manager;
 
     event Deposit(
         address indexed sender,
@@ -45,7 +49,19 @@ contract DirectUBQFarmer is ReentrancyGuard {
     );
 
 
-    constructor() {
+    constructor(address _manager, address base3Pool, address depositzap) {
+        manager = IUbiquityAlgorithmicDollarManager(_manager); // 0x4DA97a8b831C345dBe6d16FF7432DF2b7b776d98
+        BondingV2 = manager.bondingContractAddress(); // 0xC251eCD9f1bD5230823F9A0F99a44A87Ddd4CA38
+        BondingShareV2 = manager.bondingShareAddress(); // 0x2dA07859613C14F6f05c97eFE37B9B4F212b5eF5
+        UAD3CRVf = manager.stableSwapMetaPoolAddress(); // 0x20955CB69Ae1515962177D164dfC9522feef567E
+        UAD = manager.dollarTokenAddress(); // 0x0F644658510c95CB46955e55D7BA9DDa9E9fBEc6
+        UBQ = manager.governanceTokenAddress(); // 0x4e38D89362f7e5db0096CE44ebD021c3962aA9a0
+        DepositZapUAD = depositzap; // 0xA79828DF1850E8a3A3064576f380D90aECDD3359;
+        //Ideally, DepositZap contract in CurveFi should have interface to fetch 3 base token, but they do not.
+        //Hence fetching 3 token from 3basePool contract, which is 0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7
+        DAI = IStableSwap3Pool(base3Pool).coins(0); //0x6B175474E89094C44Da98b954EedeAC495271d0F
+        USDC = IStableSwap3Pool(base3Pool).coins(1); //0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+        USDT = IStableSwap3Pool(base3Pool).coins(2); //0xdAC17F958D2ee523a2206206994597C13D831ec7
     }
 
     function onERC1155Received(
@@ -180,7 +196,7 @@ contract DirectUBQFarmer is ReentrancyGuard {
 
     }
 
-    function isMetaPoolCoin(address token) public pure returns (bool) {
+    function isMetaPoolCoin(address token) public view returns (bool) {
         return (token == USDT || token == USDC || token == DAI || token == UAD);
     }
 
